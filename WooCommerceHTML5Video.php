b09b7047c4e61a79f09ada4e57c9af77b6fce9a6
Carlos Guzman <caralbgm@gmail.com>
2016-11-25 09:51:58 -0500

    Preparation of release 1.7.6

--- WooCommerceHTML5Video.php
+++ WooCommerceHTML5Video.php
@@ -5,7 +5,7 @@
  * Description: Show videos in product pages of your WooCommerce store using HTML5. It supports Mp4, Ogg, Webm and embedded videos from websites like youtube or vimeo
  * Author: Webilop
  * Author URI: http://www.webilop.com
- * Version: 1.7.5
+ * Version: 1.7.6
  * License: GPLv2 or later
  */
 namespace WooCommerceHTML5Video;
